c2a56c905776724a0676d83b7d38bda3aea59eb9
Aleks B <aleksandir@me.com>
2023-12-15 21:45:54 +1030

    Refactor main function to improve code readability and fix potential bugs

--- main.py
+++ main.py
@@ -26,33 +26,31 @@ def get_page(url):
 
 
 def main():
-    # searchTerm = input("Enter the product you would like to search for: ").strip()
-    searchTerm = "USB fingerprint reader"
-    for char in searchTerm:
-        if char == " ":
-            searchTerm = searchTerm.replace(" ", "+")
+    searchTerm = "USB fingerprint reader".replace(" ", "+")
 
     # Get the page
     page = get_page(f"https://www.pccasegear.com/search?query={searchTerm}&page=1")
 
-    # Check if the page loaded correctly
+    # Search elements for the first product and its name, price and url
     product_name_element = page.find("span", class_="product-model")
-    product_price_element = page.find("div", {"class": "price-box"}).find(
-        "div", {"class": "price"}
+    product_price_element = page.find("div", class_="price-box").find(
+        "div", class_="price"
     )
-    # pull the url for the product
-    url = page.find("a", class_="product-image")
-    url = "https://www.pccasegear.com" + url["href"]
-    if product_name_element is None or product_price_element is None:
+    url_element = page.find("a", class_="product-image")
+
+    # check if the page loaded correctly by checking if the elements exist
+    if not all([product_name_element, product_price_element, url_element]):
         print("The page did not load correctly.")
         with open("error_page.html", "w", encoding="utf-8") as f:
             f.write(page.prettify())
         return
 
-    # If the page loaded correctly, extract the product name and price
+    # If the page loaded correctly, extract the product name, price and url
     product_name = product_name_element.text.strip()
     product_price = product_price_element.text.strip()
+    url = "https://www.pccasegear.com" + url_element["href"]
 
+    # Print the product name, price and url
     print(f"Product Name: {product_name}")
     print(f"Price: {product_price}")
     print(f"URL: {url}")
