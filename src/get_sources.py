4fc155488ad994b056899d62c9a5b7b3583eb580
Aleks B <aleksandir@me.com>
2023-12-16 10:39:42 +1030

    Add get_sources.py to scrape product URLs from category page

---
+++ src/get_sources.py
@@ -0,0 +1,54 @@
+from bs4 import BeautifulSoup
+from main import get_page
+from selenium import webdriver
+from selenium.webdriver.firefox.options import Options
+
+# scrape ul tag
+# scrape inner ul tag
+# scrape li tag for product url
+# if li tag has ul tag, recurse to get product url and not the category page as the info wont be present
+# stop at end of ul tag
+
+# save product url to list
+# once list is complete, save to sources.txt file with a link on each line
+
+
+def get_product_urls(url):
+    """
+    Gets the product URLs from a category page.
+
+    Args:
+        url (str): The URL of the category page.
+
+    Returns:
+        list: A list of product URLs.
+    """
+
+    page = get_page(url)
+
+    product_urls = []
+    product_url_elements = page.findAll("li")
+
+    for product_url_element in product_url_elements:
+        if product_url_element.find("li") is None:
+            url = product_url_element.find("a")["href"]
+            if (
+                url.startswith("https://www.pccasegear.com/category/")
+                and url not in product_urls
+            ):
+                product_urls.append(url)
+
+    return product_urls
+
+
+def main():
+    SITEMAP_URL = "https://www.pccasegear.com/site_map"
+    product_urls = get_product_urls(SITEMAP_URL)
+
+    with open("src/sources.txt", "w") as f:
+        for product_url in product_urls:
+            f.write(f"{product_url}\n")
+
+
+if __name__ == "__main__":
+    main()
