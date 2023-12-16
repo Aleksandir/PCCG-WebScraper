cb38550df38f27f61a2af3807c1d2bfd32698442
Aleks B <aleksandir@me.com>
2023-12-16 18:11:35 +1030

    Refactor code to use concurrent.futures for scraping and saving products
    Removed unused function to use website search bar to scrape catagories

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+import concurrent.futures
 import json
 import sys
 
@@ -101,106 +102,26 @@ def scrape_product_category_page(url):
     return products
 
 
-#! This function is not complete
-#! once search page circles back to the first page, it will not stop loading
-# The page did not load correctly. Skipping...
-# The page did not load correctly. Skipping...
-# The page did not load correctly. Skipping...
-def scrape_search_page(searchTerm):
-    """
-    Scrapes the search page of a website to retrieve product information.
-
-    Args:
-        searchTerm (str): The search term used to query the website.
-
-    Returns:
-        dict: A dictionary containing the product names as keys and their corresponding prices and URLs as values.
-    """
-    # Get the page
-    page_number = 1
-    products = {}
-    while True:
-        page = get_page(
-            f"https://www.pccasegear.com/search?query={searchTerm}&page={page_number}"
-        )
-        try:
-            page_viewing = int(
-                page.find(
-                    "a", class_="ais-Pagination-link ais-Pagination-link--selected"
-                ).text
-            )
-        except AttributeError:
-            if page_number == 1:
-                print("No results found.")
-            else:
-                print("The page did not load correctly. Skipping...")
-                page_number += 1
-                continue
-
-        # test if the current page is the page we are viewing
-        # otherwise we have reached the end of the search results
-        if page_viewing == page_number:
-            print(f"Page {page_number} loaded.")
-        else:
-            break
-
-        # Search elements for the first product and its name, price and url
-        product_name_elements = page.findAll("span", class_="product-model")
-        product_price_elements = page.findAll("div", class_="price-box")
-        url_elements = page.findAll("a", class_="product-image")
-
-        # check if the page loaded correctly by checking if the elements exist
-        if not all([product_name_elements, product_price_elements, url_elements]):
-            print("The page did not load correctly.")
-            with open("error_page.html", "w", encoding="utf-8") as f:
-                f.write(page.prettify())
-            break
-
-        # If the page loaded correctly, extract the product names, prices and urls
-        # save them to a dictionary
-        for name_element, price_element, url_element in zip(
-            product_name_elements, product_price_elements, url_elements
-        ):
-            parsed_url = urlparse(url_element["href"])
-            product_name = parsed_url.path.split("/")[-1].replace("-", " ")
-
-            # If the product name is empty, skip it
-            if product_name.strip() == "":
-                continue
-            else:
-                products[product_name.strip()] = {
-                    "price": price_element.find("div", class_="price").text,
-                    "url": "https://www.pccasegear.com" + url_element["href"],
-                }
-
-        # Increment page number
-        page_number += 1
-
-    return products
+def scrape_and_save(url):
+    print(f"Scraping {url.split('/')[-1].replace('-', ' ').strip()}...")
+    products = scrape_product_category_page(url)
+    save_data(products)
+    print(f"{len(products)} products saved")
+    return len(products)
 
 
 def main():
-    save_count = 0
     SOURCES_FILE = "src/sources.txt"
     print("Scraping PCCaseGear...")
 
-    search_terms = get_search_pages(SOURCES_FILE)
+    site_urls = get_search_pages(SOURCES_FILE)
 
-    for term in search_terms:
-        if term.startswith("https://"):
-            print(f"Scraping {term.split("/")[-1].replace("-", " ").strip()}...")
-            products = scrape_product_category_page(term)
-            save_data(products)
-            print(f"{len(products)} products saved")
-            save_count += len(products)
-        else:
-            #! This is not complete and will not work
-            print(f"Searching for {term}...")
-            products = scrape_search_page(term)
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        save_counts = executor.map(scrape_and_save, site_urls)
 
-    print(f"\n{save_count} products scraped and saved in total.")
+    total_saved = sum(save_counts)
+    print(f"\n{total_saved} products scraped and saved in total.")
 
-    # browser.quit()
     sys.exit()
 
 
