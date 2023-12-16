51ebb87276c7cc278c35f8b1fb6ca80cdef2ef73
Aleks B <aleksandir@me.com>
2023-12-16 20:43:23 +1030

    Refactor get_sources.py to improve readability and remove unused imports

--- src/get_sources.py
+++ src/get_sources.py
@@ -1,14 +1,12 @@
-from bs4 import BeautifulSoup
+import re
+
 from main import get_page
-from selenium import webdriver
-from selenium.webdriver.firefox.options import Options
 
 # scrape ul tag
 # scrape inner ul tag
 # scrape li tag for product url
 # if li tag has ul tag, recurse to get product url and not the category page as the info wont be present
 # stop at end of ul tag
-
 # save product url to list
 # once list is complete, save to sources.txt file with a link on each line
 
@@ -30,13 +28,13 @@ def get_product_urls(url):
     product_url_elements = page.findAll("li")
 
     for product_url_element in product_url_elements:
-        if product_url_element.find("li") is None:
-            url = product_url_element.find("a")["href"]
-            if (
-                url.startswith("https://www.pccasegear.com/category/")
-                and url not in product_urls
-            ):
-                product_urls.append(url)
+        link = product_url_element.find("a")
+        url = link["href"]
+        if (
+            url.startswith("https://www.pccasegear.com/category/")
+            and url not in product_urls
+        ):
+            product_urls.append(url)
 
     return product_urls
 
